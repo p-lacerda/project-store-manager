dc73015440e5d22041b209843affcd5b67b42e3c
p-lacerda <xcloudgameemail@gmail.com>
2022-02-02 02:58:16 -0300

    problema com return resolvido

--- services/SalesService.js
+++ services/SalesService.js
@@ -3,9 +3,9 @@ const salesModel = require('../models/SalesModel');
 const createSalesProduct = async (body) => {
   const id = await salesModel.createSale();
 
-  body.map(async ({ product_id: productId, quantity }) => {
-   salesModel.createSalesProduct({ productId, quantity, id });
-  });
+  body.map(async ({
+    product_id: productId, quantity,
+  }) => salesModel.createSalesProduct({ productId, quantity, id }));
 
   return id;
 };
